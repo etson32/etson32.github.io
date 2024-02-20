2d53f856065318af967bd170b008dad5b20f3cd6
Etson Ronaldao Rojas <etson32@gmail.com>
2024-02-20 12:46:41 -0500

    deploy github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({  
-    site: 'https://etson32.github.io/',
-    base: '/src/pages',});
+    site: 'https://etson32.github.io'});
